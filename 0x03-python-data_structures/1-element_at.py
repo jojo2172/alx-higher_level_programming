43fa485260006ff99290c0c8d8d57ef3faba4808
jojo2172 <95707998+jojo2172@users.noreply.github.com>
2022-07-02 00:12:44 -0700

    element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-
-def print_list_integer(my_list=[]):
-
-    for n in my_list:
-
-        print("{:d}".format(n))
+def element_at(my_list, idx):
+    return my_list[idx] if (0 <= idx < len(my_list)) else None
